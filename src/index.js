588d35e1b02088c5d8f780367916c00b8e0086a6
Nikolay Yuush <bawerek@gmail.com>
2020-06-06 18:18:27 +0300

    Modals, block-gifts, blockToDown, timeout,

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import './css/animate.css';
 import './css/bootstrap.css';
-
 import './css/reset.css';
 import './css/main.css';
+import modal from '@/module/modal';
+
+
+window.addEventListener('DOMContentLoaded', ()=>{
+		'use strict'
+		modal();
+})
