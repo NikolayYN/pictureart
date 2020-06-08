29c31968a065f8c46d0d872a2e995a299506ca26
Nikolay Yuush <bawerek@gmail.com>
2020-06-08 17:05:54 +0300

    slider is complete

--- src/index.js
+++ src/index.js
@@ -3,9 +3,14 @@ import './css/bootstrap.css';
 import './css/reset.css';
 import './css/main.css';
 import modal from '@/module/modal';
+import slider from '@/module/slider';
 
 
-window.addEventListener('DOMContentLoaded', ()=>{
+window.addEventListener('DOMContentLoaded', ()=> {
 		'use strict'
 		modal();
+		slider('.feedback-slider', '.feedback-slider-item',
+			0, 'horizontal', '.main-prev-btn', '.main-next-btn')
+		slider('.main-slider', '.main-slider-item',
+			0, 'vertical');
 })
